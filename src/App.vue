bbaa84a28b734f9eceaa9a122429f33a468f54c2
DESKTOP-D589FND\BoSheng <toyasu814@163.com>
2017-06-01 16:15:28 +0800

    引入sass，并且引入外部css

--- src/App.vue
+++ src/App.vue
@@ -27,14 +27,26 @@
 </script>
 
 <style rel="text/stylesheet" lang="scss" scoped>
+@import "./common/css/mixin.scss";
 .tab {
   display: flex;
   width: 100%;
   height: 40px;
   line-height: 40px;
+  // border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+  @include border_px(rgba(7, 17, 27, 0.1));
   .tab-item {
     flex: 1;
     text-align: center;
+    a {
+      display: block;
+      font: 200 14px/40px 'Microsoft YaHei';
+      color: rgb(77, 85, 93);
+
+      &.active {
+        color: rgb(240, 20, 20);
+      }
+    }
   }
 }
 </style>
