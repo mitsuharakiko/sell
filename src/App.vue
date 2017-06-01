663a064dabe385545bd353d13015b33b5fb2af72
DESKTOP-D589FND\BoSheng <toyasu814@163.com>
2017-06-01 14:56:49 +0800

    vue-cli 搭建项目

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <a v-link="{path: '/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/seller'}">商家</a>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Header from './components/header/header';
+
+  export default {
+    components: {
+      'v-header': Header
+    }
+  };
+</script>
+
+<style rel="text/stylesheet" lang="scss" scoped>
+.tab {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  .tab-item {
+    flex: 1;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="header">
+		This is Header!
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {};
+</script>
+
+<style type="stylesheet/stylus" lang="scss">
+</style>
